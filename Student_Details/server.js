c658d0ce9d770cd986141c4945e81443786104e1
kowsalyaMano <158285505+kowsalyaMano@users.noreply.github.com>
2025-06-25 09:15:42 +0530

    Update server.js

--- Student_Details/server.js
+++ Student_Details/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 app.use('/api/students', studentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;   //PORT=5000 in .env file
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
