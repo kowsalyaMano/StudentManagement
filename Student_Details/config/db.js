0daec7bff67b52a5bcc35ccafe0750890549b7b0
kowsalyaMano <158285505+kowsalyaMano@users.noreply.github.com>
2025-06-25 09:14:52 +0530

    Update db.js

--- Student_Details/config/db.js
+++ Student_Details/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);   //MONGO_URI=mongodb+srv://<username>:<password>@cluster0.psaavzb.mongodb.net/Student_Management?retryWrites=true&w=majority
+                                                     //use ur mongodb atlas username and password
+   
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB Connection Failed:', err.message);
